c6150631051db90c670285a9a82b976319bf9a96
Alvaro Tapia <ajtapia@miuandes.cl>
2026-05-13 20:12:13 -0400

    Mont vs Cap ready

--- Graphs/Montesco vs Capuleto/solution.py
+++ Graphs/Montesco vs Capuleto/solution.py
@@ -1,74 +1,125 @@
-def read_input():
-    cases = []
-    m = int(input().strip())
-    blank = input()
-    for i in range(m):
-        people = {}
-        n = int(input().strip())
-        for j in range(n):
-            people[j+1] = {"enemies": [], "friends": []}
-            line = list(map(int, input().strip().split()))
-            if line[0] != 0:
-                people[j+1]["enemies"] = line[1:]
-        cases.append(people)
-        if i < m - 1:
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.color = None
+        self.visited = False
+        
+class Graph:
+    def __init__(self):
+        self.nodes = {}
+        self.adjacency = {}
+        
+    
+    def add_node(self, key):
+        self.nodes[key] = Node(key)
+        
+    def get_node(self, key):
+        return self.nodes[key]
+    
+    def add_edge(self, n1, n2):
+        if n1 not in self.adjacency:
+            self.adjacency[n1] = []
+        self.adjacency[n1].append(n2)
+        
+    def component_dfs(self, start_node, visited=None):
+        if visited is None:
+            visited = set()
+            
+        component = []
+        
+        if start_node not in visited:
+            component.append(start_node)
+            visited.add(start_node)
+            for node in self.adjacency.get(start_node, []):
+                if node not in visited:
+                    component.extend(self.component_dfs(node, visited))
+                    
+        return component
+        
+    def get_connected_components(self):
+        for node in self.nodes:
+            if node not in self.adjacency:
+                self.adjacency[node] = []
+        components = []
+        visited = set()
+        for node in self.nodes:
+            if node not in visited:
+                component = self.component_dfs(node, visited)
+                components.append(component)
+        return components
+    
+    def color_dfs(self, start_node, curr_color):
+        self.nodes[start_node].color = curr_color
+        for neigh in self.adjacency.get(start_node, []):
+            if self.nodes[neigh].color == None:
+                if not self.color_dfs(neigh, 1 - curr_color):
+                    return False
+            elif self.nodes[neigh].color == curr_color:
+                return False
+        return True
+    
+    def is_bipartite(self, component):
+        for node in component:
+            if self.nodes[node].color is None:
+                if not self.color_dfs(node, 0):
+                    return False
+        return True        
+
+    def get_max_guests(self, component):
+        cero_count = 0
+        one_count = 0
+        for node in component:
+            if self.nodes[node].color == 0:
+                cero_count += 1
+            else:
+                one_count += 1
+        return max(cero_count, one_count)
+            
+        
+class Solution:
+    def __init__(self):
+        cases = self.read_input()
+        self.solve(cases)
+    
+    def read_input(self):
+        cases = []
+        num_cases = int(input())
+        for c in range(num_cases):
+            case = {}
             blank = input()
-    return cases
+            num_people = int(input())       # <= 200
+            for i in range(1, num_people + 1):
+                enemies = list(map(int, input().strip().split()))
+                enemies.pop(0)
+                case[i] = enemies
+            cases.append(case)
+        return cases
+ 
+    def solve_case(self, case):
+        case_graph = Graph()
+        for person in case:
+            case_graph.add_node(person)
+            
+        for person in case:
+            for enemy in case[person]:
+                if enemy in case:
+                    case_graph.add_edge(person, enemy)
+                    case_graph.add_edge(enemy, person)
 
-def fill_friends(people: dict[int: dict["enemies": list], "friends": list]):
-    for person in people.keys():
-        for enemy in people[person]["enemies"]:
-            for friend in people[enemy]["enemies"]:
-                if friend not in people[person]["enemies"] and person not in people[friend]["enemies"] and friend != person and friend not in people[person]["friends"]:
-                    people[person]["friends"].append(friend)
-                    people[friend]["friends"].append(person)
-    return people
-
-def fill_enemies(people: dict[int: dict["enemies": list], "friends": list]):
-    for person in people.keys():
-        for enemy in people[person]["enemies"]:
-            people[enemy]["enemies"].append(person)
-    return people
-
-def check_solution(guests: list):
-    for guest in guests:
-        pass
-    return
-
-def compute_max_guests(people: dict[int: dict["enemies": list], "friends": list]):
-    max_guests = 0
-    for i in range(1, len(people.keys()) + 1):
-        banned = set()
-        guests = []
-        first_guest = people[i]
-        guests.append(i)
-        for enemy in first_guest["enemies"]:
-            banned.add(enemy)
-        for person in people.keys():
-            if person != i and person not in banned:
-                guests.append(person)
-                for enemy in people[person]["enemies"]:
-                    banned.add(enemy)
-        if len(guests) > max_guests and check_solution(guests):
-            max_guests = len(guests)
-    print(max_guests)
-
-def main():
-    cases = read_input()
-    for case in cases:
-        people = fill_friends(case)
-        people = fill_enemies(people)
-        compute_max_guests(people)
-
-
-if __name__ == "__main__":
-    main()
-
-
-'''
-NOTES
-
-casi listo, falta verificar si es posible:
-    b es enemigo de a, pero a es amigo de b (relaciones)
-
-'''
+        components = case_graph.get_connected_components()
+        
+        total_guests = 0
+        for component in components:
+            if case_graph.is_bipartite(component):
+                total_guests += case_graph.get_max_guests(component)
+        
+        return total_guests
+                
+                
+    def solve(self, cases):
+        for case in cases:
+            print(self.solve_case(case))
+        
+        
+solution = Solution()
