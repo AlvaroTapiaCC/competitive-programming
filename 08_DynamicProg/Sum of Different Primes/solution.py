0c7a6579f22d5a1a1a1ae030892b47f55d243003
Alvaro Tapia <ajtapia@miuandes.cl>
2026-05-31 18:58:23 -0400

    sum of diff primes ready 4/5

--- 08_DynamicProg/Sum of Different Primes/solution.py
+++ 08_DynamicProg/Sum of Different Primes/solution.py
@@ -1,18 +1,54 @@
+import math
+
 class Solution:
     def __init__(self):
-        cases = self.read_input()
+        cases = self.readInput()
+        self.primes = []
+        for n in range (2, 1120):
+            if self.isPrime(n):
+                self.primes.append(n)
+        self.dp = self.buildDP()               
         self.solve(cases)
-    
-    def read_input(self):
+        
+    def isPrime(self, n):
+        if n < 2:
+            return False
+        elif n == 2:
+            return True
+        else:
+            for i in range(2, round(n**0.5) + 1):
+                if n % i == 0:
+                    return False
+            return True
+        
+    def readInput(self):
         cases = []
+        while True:
+            case = tuple(map(int, input().strip().split()))
+            if case == (0, 0):
+                break
+            cases.append(case)
         return cases
     
-    def solve_case(self, case):
-        return
+    def buildDP(self):
+        MAX_SUM = 1121
+        MAX_K = len(self.primes)
+        dp = [[0] * (MAX_K + 1) for _ in range(MAX_SUM)]
+        dp[0][0] = 1 
+        
+        for prime in self.primes:
+            for s in range(MAX_SUM - 1, prime - 1, -1):
+                for k in range(MAX_K, 0, -1):
+                    dp[s][k] += dp[s - prime][k - 1]
+        return dp
+    
+    def solveCase(self, case):
+        return self.dp[case[0]][case[1]]
     
     def solve(self, cases):
         for case in cases:
-            print(self.solve_case(case))
+            print(f"{self.solveCase(case)}")
+
 
 if __name__ == "__main__":
     solution = Solution()
